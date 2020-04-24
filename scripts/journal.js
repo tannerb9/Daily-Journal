e59863b24c10a0b3da4124fb242081770730a9dc
tannerb9 <tanner-brainard@hotmail.com>
2020-04-24 13:37:24 -0500

    merged master & removed excess code

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,25 +1,4 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-
-const journal_entry = {
-  date: "",
-  concepts_covered: "",
-  briefing: "",
-  mood: "",
-};
-
-// Define an appropriately named variable that will have the value of an array.
 let entries = [];
-
-// Once you define that variable and give it a default value of a blank array,
-// use the push() method to add the journalEntry object you defined above to the array.
-
-entries.push(journal_entry);
-
-// Take a few minutes and define two or three more JavaScript objects to represent journal
-// entries for some other things that you have learned about so far at NSS.
 
 const flexbox = {
   date: "03/27/20",
@@ -37,9 +16,5 @@ const javascript = {
   mood: "tranquil",
 };
 
-// Add those new objects to your journal entries array.
-
 entries.push(flexbox);
 entries.push(javascript);
-
-console.log(entries);
