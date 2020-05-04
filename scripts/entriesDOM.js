84f35617cd1754749a133d244bdfb89e847de5a2
tannerb9 <tanner-brainard@hotmail.com>
2020-05-04 13:30:51 -0500

    implemented ES6 modularization

--- scripts/entriesDOM.js
+++ scripts/entriesDOM.js
@@ -1,6 +1,10 @@
+import makeJournalEntryComponent from "./entryComponent.js";
+
 const renderJournalEntries = (entries) => {
   for (let index = 0; index < entries.length; index++) {
     let record = document.querySelector(".entryLog");
     record.innerHTML += makeJournalEntryComponent(entries[index]);
   }
 };
+
+export default renderJournalEntries;
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,1 +1,4 @@
+import API from "./data.js";
+import renderJournalEntries from "./entriesDOM.js";
+
 API.getJournalEntries().then(renderJournalEntries);
