f1545acdd4c4b7eddcbf89c5ffa2f81a60569957
tannerb9 <tanner-brainard@hotmail.com>
2020-05-06 13:53:38 -0500

    made DOM render latest entry upon submission

--- scripts/entriesDOM.js
+++ scripts/entriesDOM.js
@@ -2,8 +2,9 @@ import makeJournalEntryComponent from "./entryComponent.js";
 // import API from "./data.js";
 
 const renderJournalEntries = (entries) => {
+  let record = document.querySelector(".entryLog");
+  record.innerHTML = "";
   for (let index = 0; index < entries.length; index++) {
-    let record = document.querySelector(".entryLog");
     record.innerHTML += makeJournalEntryComponent(entries[index]);
   }
 };
--- scripts/journal.js
+++ scripts/journal.js
@@ -4,18 +4,34 @@ import renderJournalEntries from "./entriesDOM.js";
 // Use a conditional to ensure no blank entries
 document.querySelector("#submitButton").addEventListener("click", (event) => {
   event.preventDefault();
-  debugger;
-  const date = document.querySelector("#journalDate").value;
-  const concepts = document.querySelector("#conceptsCovered").value;
-  const journal = document.querySelector("textarea").value;
-  const mood = document.querySelector("#moodForTheDay").value;
 
-  const newJournalEntry = createJournalEntry(date, concepts, journal, mood);
-  API.saveJournalEntry(newJournalEntry)
-    .then((dataJS) => {
-      return API.getJournalEntries();
-    })
-    .then((entries) => renderJournalEntries(entries));
+  if (
+    document.querySelector("#journalDate").checkValidity() == false ||
+    document.querySelector("textarea").checkValidity() == false
+  ) {
+    window.alert("Please use valid characters.");
+  } else {
+    if (
+      document.querySelector("#journalDate").value !== "" &&
+      document.querySelector("#conceptsCovered").value !== "" &&
+      document.querySelector("textarea").value !== "" &&
+      document.querySelector("#moodForTheDay").value !== ""
+    ) {
+      const date = document.querySelector("#journalDate").value;
+      const concepts = document.querySelector("#conceptsCovered").value;
+      const journal = document.querySelector("textarea").value;
+      const mood = document.querySelector("#moodForTheDay").value;
+
+      const newJournalEntry = createJournalEntry(date, concepts, journal, mood);
+      API.saveJournalEntry(newJournalEntry)
+        .then((dataJS) => {
+          return API.getJournalEntries(dataJS);
+        })
+        .then((entries) => renderJournalEntries(entries));
+    } else {
+      window.alert("Please complete all fields.");
+    }
+  }
 });
 
 const createJournalEntry = (date, concepts, journal, mood) => ({
